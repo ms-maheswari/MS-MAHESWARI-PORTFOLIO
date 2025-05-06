85041d94772ffe72a977cd9a66cbbca89068fc28
ms-maheswari <mahes7439@gmail.com>
2025-05-06 19:39:14 +0530

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project2.png";
 import project3 from "../assets/projects/project3.png";
 import project4 from "../assets/projects/project4.png";
 import project5 from "../assets/projects/project5.png";
-
+import project6 from "../assets/projects/project6.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With hands-on experience, I have honed my skills in front-end technologies like React and Tailwind CSS, as well as back-end technologies like Node.js, MySQL, Express.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -16,7 +16,8 @@ export const EXPERIENCES = [
     company: "Zenanvibe",
     description: `Gained practical expertise in React Tailwind CSS for rapid styling.
     `,
-    point2:"Mastered responsiveness to ensure seamless user experiences across various devices.",
+    point2:
+      "Mastered responsiveness to ensure seamless user experiences across various devices.",
     technologies: ["Javascript", "React.js", "Tailwind CSS"],
   },
   {
@@ -24,16 +25,18 @@ export const EXPERIENCES = [
     role: "Web Developer Intern",
     company: "Qantler Technologies",
     description: `Gained extensive proficiency in validation, input types, JavaScript functions, styling.`,
-    point2:"The creation of forms with validation and CRUD (Create, Read, Update, Delete) operations",
-    technologies: ["Html","CSS","Javascript"],
+    point2:
+      "The creation of forms with validation and CRUD (Create, Read, Update, Delete) operations",
+    technologies: ["Html", "CSS", "Javascript"],
   },
   {
     year: "March 2023 - May 2023",
     role: "Mern Stack Intern",
     company: "Nullclass",
     description: `Crafted and launched a responsive web application utilizing the MERN stack with user interaction`,
-    point2:"Integrated user authentication and authorization functionalities, empowering users to securely establish accounts, log in, and oversee their profiles.",
-    technologies: ["React", "Node.js","Express.js","MongoDB"],
+    point2:
+      "Integrated user authentication and authorization functionalities, empowering users to securely establish accounts, log in, and oversee their profiles.",
+    technologies: ["React", "Node.js", "Express.js", "MongoDB"],
   },
 ];
 
@@ -43,37 +46,47 @@ export const PROJECTS = [
     image: project1,
     description:
       "The Quotes Generator App is a  application that fetches random inspirational quotes from an external API. ",
-    point2 :"Users can click a button to load a new quote, providing a seamless and engaging experience.",
-    technologies: ["React","Javascript","Tailwind CSS","API"],
+    point2:
+      "Users can click a button to load a new quote, providing a seamless and engaging experience.",
+    technologies: ["React", "Javascript", "Tailwind CSS", "API"],
     live: "https://quotes-generator-nu-two.vercel.app/",
-    github:"https://github.com/ms-maheswari/Quotes-Generator",
+    github: "https://github.com/ms-maheswari/Quotes-Generator",
   },
   {
     title: "Mind Scribe",
     image: project2,
     description:
       "The MindScribe Project is a feature-rich note-taking application that allows users to create, pin, and organize notes efficiently. ",
-    point2 : "With a clean and intuitive interface, users can securely log in to access their notes, making it an ideal tool for personal or professional use.",
-    technologies: ["React","Node.js","Express.js","MongoDB"],
+    point2:
+      "With a clean and intuitive interface, users can securely log in to access their notes, making it an ideal tool for personal or professional use.",
+    technologies: ["React", "Node.js", "Express.js", "MongoDB"],
     live: "https://mind-scribe-xi.vercel.app/",
     github: "https://github.com/ms-maheswari/Mind_Scribe",
   },
   {
-    title: "Sonar Signal Classification",
+    title: "Forest Fire Prediction",
     image: project3,
     description:
-       "The Sonar Signal Classifier is a machine learning project aimed at classifying sonar signals as either rocks or mines.  This project can be integrated into a web interface where users can input sonar data and receive classification results",
-    point2: "Using logistic regression, the model analyzes the sonar data to predict the classification.",
-    technologies: ["Python", "Flask", "Scikit-learn", "NumPy", "Pandas"],
-    live:" ",
-    github:"https://github.com/ms-maheswari/Sonar-Signal-Classifier",
+      "An intelligent system that predicts forest fire risks using real-time weather and vegetation data, aimed at preventing disasters before they happen.",
+    point2:
+      "Combining machine learning with live weather APIs and Power BI visualizations, it helps forest authorities make informed decisions with timely alerts.",
+    technologies: [
+      "React",
+      "Flask",
+      "Python",
+      "Machine Learning",
+      "OpenWeatherMap API",
+    ],
+    live: " ",
+    github: "https://github.com/ms-maheswari/Forest-Fire-Prediction",
   },
   {
     title: "News Aggregator Website",
     image: project4,
     description:
       "The News Pulse Project is a responsive news aggregation website. It fetches the latest news based on different categories using the News API and presents them in a user-friendly layout. ",
-      point2:"Users can explore various news categories such as sports, technology, and business.",
+    point2:
+      "Users can explore various news categories such as sports, technology, and business.",
     technologies: ["React", "Tailwind CSS", "API"],
     live: "https://news-pulse-phi.vercel.app/",
     github: "https://github.com/ms-maheswari/NewsPulse",
@@ -83,10 +96,30 @@ export const PROJECTS = [
     image: project5,
     description:
       "A platform where users can sign up, log in, and interact with a community by asking questions, providing answers, sharing posts, and more. ",
-      point2: "The app includes a chatbot for assistance, email verification via OTP, and various membership plans offering different levels of access.",
-    technologies: ["React.js","Node.js","Express.js","MongoDB","JWT","SMTP"],
+    point2:
+      "The app includes a chatbot for assistance, email verification via OTP, and various membership plans offering different levels of access.",
+    technologies: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "JWT",
+      "SMTP",
+    ],
     live: "https://stack-clone-frontend.vercel.app/",
     github: "https://github.com/ms-maheswari/stack-clone",
+  },
+  {
+    title: "Bike Care Hub",
+    image: project6,
+    description:
+      "Bike Care Hub is a smart service management system designed to streamline bike service bookings, track maintenance history, and enhance customer experience.",
+    point2:
+      "With real-time updates, secure login, and service reminders, it bridges the gap between bike owners and service centers for efficient maintenance.",
+    technologies: ["React", "Node.js", "Express.js", "MongoDB"],
+  
+  live: "https://bikecarehub.vercel.app/",
+  github: "https://github.com/ms-maheswari/bike-care-hub",
   },
 ];
 
@@ -96,7 +129,7 @@ export const CONTACT = {
 
 export const EDUCATION = [
   {
-    year: "2021 - Present",
+    year: "2021 - 2025",
     std: "B.E - Computer Science",
     marks: "8.5 GPA",
     institute: "Francis Xavier Engineering College",
